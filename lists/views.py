a5e03718dca6f84aee135909d1c8b2107d4f2adb
dthylacetate <dthylacetate@qq.com>
2026-05-18 16:39:57 +0800

    能成功把条目展示

--- lists/views.py
+++ lists/views.py
@@ -6,8 +6,10 @@ def home_page(request):
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
         return redirect('/')
+    
+    items= Item.objects.all()
 
-    return render(request, 'home.html')
+    return render(request, 'home.html',{'items': items})
  
 
 # Create your views here.
